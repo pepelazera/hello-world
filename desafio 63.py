d7be5fbcea09017c8469dede1c7e5120a62da405
Pedro Lazera Jr <145514613+pepelazera@users.noreply.github.com>
2025-03-21 12:01:47 -0300

    Desafio 63. Rotação Áurea
    Esse foi com certeza o desafio mais difícil que eu fiz, justamente por não saber direito como funcionava a fórmula da sequência de Fibonnaci e nunca ter estudado isso na minha vida. Tive que olhar a resolução para somente depois entender como funcionava e fazer sem pesquisar ou ver vídeos sobre ajuda.

---
+++ desafio 63.py
@@ -0,0 +1,19 @@
+print("-"*30)
+print("Sequência de Fibonacci 💀")
+print("-"*30)
+
+n = int(input("Quantos termos você quer mostrar ? "))
+t1 = 0
+t2 = 1
+cont = 3
+
+print(f"{t1} → {t2}", end='')
+
+while cont <= n:
+    t3 = t1 + t2
+    print(f" → {t3}", end='')
+    t1 = t2
+    t2 = t3
+    cont += 1
+print(" → FIM")
+print("-"*30)
