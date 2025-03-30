7968450614a72adc1038d1afc2071111103ddec3
Pedro Lazera Jr <145514613+pepelazera@users.noreply.github.com>
2025-03-30 15:25:45 -0300

    aulas 16 sobre tuplas
    Aqui estão alguns dos meus aprendizados sobre tuplas. Próximos arquivos serão sobre os exercícios que eu fiz sobre essas aulas. Mandei dois arquivos para poder salvar tudo que eu aprendi de maneira organizada.

---
+++ aula 16. Tuplas.py
@@ -0,0 +1,16 @@
+lanche = ('hamburguer', 'taco', 'pizza', 'brownie')
+
+print(sorted(lanche))
+print(lanche)
+
+#for comida in lanche:
+#   print(f'Eu vou comer {comida}')
+
+#for cont in range(0, len(lanche)):
+#    print(f'Eu vou comer {lanche[cont]} na posição {cont}')
+
+#for pos, comida in enumerate(lanche):
+#  print(f'Eu vou comer {comida} na posição {pos}')
+
+
+#Tuplas são imutáveis.
---
+++ aula 16.2.py
@@ -0,0 +1,10 @@
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = b + a 
+print(len(c))
+print(c.count(2))
+print(c.index(5, 1))
+
+pessoa = ('Gustavo', 39, 'M', 99.88)
+#del(pessoa). Usa para deletar uma tupla. Obs: Não é possível apagar somente 1 elemento da tupla, somente a tupla inteira.
+print(pessoa)
