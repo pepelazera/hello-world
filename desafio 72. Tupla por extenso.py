7d15fc1ac168874d32e18f135a159d973774a6f0
Pedro Lazera Jr <145514613+pepelazera@users.noreply.github.com>
2025-03-31 12:06:13 -0300

    Aperfeiçoamento do desafio 72
    Na resolução, o professor pediu para criar um comando no qual ele perguntasse se o usuário queria repetir ou não. Como eu quis fazer da melhor maneira possível, mas ainda não tinha conhecimento para fazer o que eu buscava, pedi uma dica ao chatgpt e ele me deu o uso do comando exit(). Antes disso, já tinha tentado fazer somente com o break e não tinha dado certo da maneira que eu queria que fosse, mas com o exit eu consegui.

--- desafio 72. Tupla por extenso.py
+++ desafio 72. Tupla por extenso.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 cont = ['zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 
     'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte']
 
@@ -5,10 +7,28 @@ while True:
     numero0a20 = int(input('Digite um número entre 0 a 20: '))
 
     if numero0a20 < 0 or numero0a20 > 20:
-        print('Por favor, digite números válidos.')
+        print('Por favor, digite números válidos.\n')
         print('')
         continue
-    else:
-        print('')
-        print(f'Você digitou o número {cont[numero0a20]}.')
-        break
+
+    print(f'Você digitou o número {cont[numero0a20]}.')
+
+
+#Loop para garantir que o usuário insira uma resposta válida
+    while True:
+        resposta = str(input('Deseja continuar (S/N) ? ')).strip().upper()
+
+        if resposta == 'S':
+            print('\nReiniciando programa...\n')
+            print('')
+            sleep(1.3)
+            break  # Sai do loop de validação e reinicia o programa
+        elif resposta == 'N':
+            sleep(1.3)
+            print('Por hoje é só! Muito obrigado!')
+            exit() # Sai completamente do programa
+        else:
+            print('Opção inválida! Digite apenas S ou N.')
+            print('')
+            sleep(0.5)
+            continue
