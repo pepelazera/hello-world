9b4d24e23dc804f1e671e2cf10df84a93d3da2c6
Pedro Lazera Jr <145514613+pepelazera@users.noreply.github.com>
2025-03-31 18:32:29 -0300

    Times Brasileirão
    Nesse código criei algumas listas e testei meu conhecimento baseado nas aulas anteriores. Depois vou pesquisar como faço para tirar esses parênteses e aspas no resultado do terminal, mas por enquanto irei enviar como commit dessa maneira.

---
+++ desafio 73. Brasileirão.py
@@ -0,0 +1,27 @@
+lista = ('Palmeiras', 'Fortaleza', 'Internacional', 'Flamengo', 'São Paulo', 'Cruzeiro', 'Bahia', 'Corinthians', 'Vitória', 'Vasco', 'Juventude', 'Grêmio', 'Santos')
+
+print('-=' * 14)
+print(f'Listas de time brasileirão: {lista}')
+print('-=' * 14)
+
+print('')
+
+print('-=' * 14)
+print(f'Os 5 primeiros são: {lista[0:5]}')
+print('-=' * 14)
+
+print('')
+
+print('-=' * 14)
+print(f'Os 4 útilmos são: {lista[9:13]}')
+print('-=' * 14)
+
+print('')
+
+print('-=' * 14)
+print(f'Times em ordem alfabética: {sorted(lista)}')
+print('-=' * 14)
+
+print('')
+
+print(f'O {lista[8]} está na oitava posição.')
