9eb12113a3ecaa08ce068a51941fbec58ffc65eb
Pedro Lazera Jr <145514613+pepelazera@users.noreply.github.com>
2025-04-02 16:04:53 -0300

    challenger 77. Spell words

---
+++ desafio 77. Soletrando palavras.py
@@ -0,0 +1,8 @@
+palavras = ('aprender', 'programar', 'linguagem', 'python', 'curso', 'gratis', 'estudar', 'praticar', 'trabalhar', 'mercado', 'programador', 'futuro')
+vogal = 'aeiou'
+
+for p in palavras:
+    print(f'\nNa palavra {p.upper()} temos ', end='')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+             print(f'{letra}', end=' ')
