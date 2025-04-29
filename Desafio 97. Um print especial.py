968cd27c8851f3077261524448d5a6a11e4f4cd0
Pedro Lazera Jr <pepelazera@gmail.com>
2025-04-29 11:22:24 -0300

    Print de maneira simples e organizada.

---
+++ Desafio 97. Um print especial.py
@@ -0,0 +1,9 @@
+def mensagem(msg):
+    print('~' * (len(msg)+4))
+    print(f'  {msg}')
+    print('~'* (len(msg)+4))
+
+
+mensagem('Gustavo Guanabara')
+mensagem('Cuso de Python no youtube')
+mensagem('CeV')
