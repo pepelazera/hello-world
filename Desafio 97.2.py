4418b089d392e2336b5a68adb3883fd4c479b363
Pedro Lazera Jr <pepelazera@gmail.com>
2025-04-29 11:26:13 -0300

    Create Desafio 97.2.py

---
+++ Desafio 97.2.py
@@ -0,0 +1,10 @@
+def mensagem(msg):
+tam = len(msg) + 4
+print('~' * tam)
+print(f'  {msg}')
+print('~' * tam)
+
+# Programa principal
+print('Gustavo Guanabara')
+print('Curso de Python no Youtube')
+print('CeV')
