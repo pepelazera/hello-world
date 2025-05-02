4b9965dbeceab7c9f2419695f905840736347193
Pedro Lazera Jr <pepelazera@gmail.com>
2025-05-02 12:45:00 -0300

    Update Desafo 99. Função que descobre o maior.py

--- Desafo 99. Função que descobre o maior.py
+++ Desafo 99. Função que descobre o maior.py
@@ -1,12 +1,12 @@
 from time import sleep
 def parametros(* num):
     quant = len(num)
-    if quant == 0:
+    if quant == 0: # Identificando o valor 0
         print('-'*50)
         print('Foram informados 0 valores ao todo.')
         print('O maior valor informado foi 0')
         sleep(0.5)
-    else:
+    else: # Separando os números normais do 0
         print('-'*50)
         print(f"{' '.join(map(str, num))} Foram informados {quant} valores ao todo.")
         print(f"O maior valor informado foi {max(num)}.")
