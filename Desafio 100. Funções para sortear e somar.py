79575a91bd3fa7108003feca591f58179da9935e
Pedro Lazera Jr <pepelazera@gmail.com>
2025-05-02 12:43:51 -0300

    Update Desafio 100. Funções para sortear e somar.py

--- Desafio 100. Funções para sortear e somar.py
+++ Desafio 100. Funções para sortear e somar.py
@@ -1,19 +1,19 @@
 from random import randint
 numeros = list()
 
-def sorteia():
+def sorteia(): # Função dos valores gerais
     for n in range(5):
         numaleat = randint(1,10)
         numeros.append(numaleat)
     print(f"Sorteando {len(numeros)} valores digitados: {' '.join(map(str, numeros))}")
 
-def paressoma():
+def paressoma(): # Função que faz a identificação de valores pares.
     pares = list()
     for v in numeros:
         if v % 2 == 0:
             pares.append(v)
     print(f"Somando os valores pares de {numeros}, temos {sum(pares)}.")
 
-
+# Programa principal
 sorteia()
 paressoma()
