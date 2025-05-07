b45e7e4435252d63fb75960bdc4bade9fea2e2cf
Pedro Lazera Jr <pepelazera@gmail.com>
2025-05-07 13:48:48 -0300

    Update Desafio 105. Analisando e gerando dicionários.py

--- Desafio 105. Analisando e gerando dicionários.py
+++ Desafio 105. Analisando e gerando dicionários.py
@@ -1,4 +1,10 @@
 def notas(* num, sit=False):
+    """
+    -> Função para analisar notas e stiuações de vários alunos.
+    :param num: Uma ou mais notas dos alunos (aceita várias);
+    :param sit: Valor opcional, indicando se deve ou não adicionar a situação;
+    :return: dicionário com várias informações sobre a situação de tal aluno.
+    """
     nt = dict()
     nt["total"] = len(num)
     nt["maior"] = max(num)
