5eb62f6f0ec91abeae097dbac1e1fc4c6fe5fff2
Pedro Lazera Jr <pepelazera@gmail.com>
2025-05-07 15:59:15 -0300

    Desafio 106 com cores mais agradáveis.
    Coloquei esse desafio 106 com cores que agradam mais meus olhos. Sem fundo branco, sem cores fortes, apenas fundo preto e cor de texto azul e roxo para ficar mais compreensível onde está cada parte do programa e também ajudará na organização.

--- Desafio 106.2.py
+++ Desafio 106.2.py
@@ -1,22 +1,37 @@
+cores = {'limpa':'\033[m',
+         'roxo': '\033[7;30;45m',
+         'amarelo':'\033[;33m',
+         'pretoebranco':'\033[7;30m',
+         'vermelho':'\033[0;30;41m',
+         'verde':'\033[0;30;42m',
+         'azul':'\033[0;34;40m',
+         'branco':'\033[0;30;47m'
+         }
+
+
 def ajuda(com):
     print('')
+    print(f"{cores['azul']}")
     help(com)
 
 
 def titulo(msg):
+    print(f"{cores['roxo']}")
     tam = len(msg)
     print('-'*tam)
     print(msg)
     print('-'*tam)
+    print(f"{cores['limpa']}")
 
 
 # Programa principal
 comando = ''
 while True:
     titulo("SISTEMA DE AJUDA PyHELP")
-    comando = str(input("Função ou Biblioteca: "))
+    comando = str(input(f"{cores['azul']}Função ou Biblioteca: "))
     if comando.upper() == "FIM":
         break
     else:
         ajuda(comando)
-print('Por hoje é só. Muito obrigado!')
+print('')
+print(f"{cores['roxo']}Por hoje é só. Muito obrigado!")
