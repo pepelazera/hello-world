a0862695ab078cad92d4ab11f9578b1ce474cb21
Pedro Lazera Jr <pepelazera@gmail.com>
2025-05-12 08:50:33 -0300

    Atualização do módulo.
    Depois de um descanso merecido, voltei para atualizar o módulo do meu programa. Reparei que tinham muitas redundâncias desnecessárias no que eu já havia feito e resolvi ajustar.

--- modulo_aula22.py
+++ modulo_aula22.py
@@ -18,17 +18,16 @@ def aumentar(n=0, t=0, formato=False):
     return res if formato is False else moeda(res)
 
 
-def moeda(valor=0, aum=0, dim=0, t=0):
-    aum = valor + (valor * aumentar(aum) / 100)
-    dim = valor - (valor * diminuir(dim) / 100)
-    moeda2 = "R$"
-    print("-" * 30)
-    print("       Resumo do valor")
-    print("-" * 30)
-    print(f"Preço analisado:    {moeda2}{valor:.2f}".replace(".",","))
-    print(f"Dobro do preço:     {moeda2}{dobro(valor):.2f}".replace(".",","))
-    print(f"Metade do preço:    {moeda2}{metade(valor):.2f}".replace(".",","))
-    print(f"20% de aumento:     {moeda2}{aum:.2f}".replace(".","."))
-    print(f"12% de redução:     {moeda2}{dim:.2f}".replace(".",","))
-    print(f"-"*30)
-    return f"{moeda2}{valor:.2f}".replace(".", ",")
+def moeda(n=0, moeda="R$"):
+    return f"{moeda}{n:>.2f}".replace(",", ".")
+
+def resumo(valor=0, taxaa=10, taxar=5):
+    print("-"*30)
+    print("Resumo do valor".center(30))
+    print("-"*30)
+    print(f"Preço analisado: \t{moeda(valor)}") # O \t é uma tabulação que serve para alinhar e ajustar o código da maneira correta.
+    print(f"Dobro do preço: \t{dobro(valor, True)}")
+    print(f"Metade do preço: \t{metade(valor, True)}")
+    print(f"{taxaa}% de aumento: \t{aumentar(valor, taxaa, True)}")
+    print(f"{taxar}% de redução: \t\t{diminuir(valor, taxar, True)}")
+    print("-"*30)
