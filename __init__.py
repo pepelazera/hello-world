4fa2a28bfb3e2f608860d2930d41060c5b2978a1
Pedro Lazera Jr <pepelazera@gmail.com>
2025-05-13 17:11:05 -0300

    Ajuste na pasta de dados.
    Não tive muito tempo hoje, então a única coisa que eu fiz foi ajustar a pasta de dados.

--- __init__.py
+++ __init__.py
@@ -1,34 +1,19 @@
-def metade(n=0, formato=False):
-    res = n/2
-    return res if formato is False else moeda(res)
+cores = {'limpa':'\033[m',
+         'roxo': '\033[34m',
+         'amarelo':'\033[33m',
+         'pretoebranco':'\033[7;30m',
+         'vermelho':'\033[31m',
+         'verde':'\033[32m',
+         }
 
 
-def diminuir(n=0, t=0, formato=False):
-    res = n - (n * t/100)
-    return res if formato is False else moeda(res)
-
-
-def dobro(n=0, formato=False):
-    res = n*2
-    return res if formato is False else moeda(res)
-
-
-def aumentar(n=0, t=0, formato=False):
-    res = n + (n * t / 100)
-    return res if formato is False else moeda(res)
-
-
-def moeda(n=0, m="R$"):
-    return f"{m}{n:.2f}"
-
-
-def resumo(valor=0, taxaa=0, taxar=0):
-    print("-"*30)
-    print("Resumo do valor".center(30))
-    print("-"*30)
-    print(f"Preço analisado: \t{moeda(valor)}")
-    print(f"Dobro do preço: \t{dobro(valor, True)}")
-    print(f"Metade do preço: \t{metade(valor, True)}")
-    print(f"{str(taxaa).rjust(4)}% de aumento: \t{aumentar(valor, taxaa, True)}")
-    print(f"{str(taxar).rjust(4)}% de redução: \t{diminuir(valor, taxar, True)}")
-    print("-"*30)
+def leiadinheiro(msg):
+    valido = False
+    while not valido:
+        entrada = str(input(msg))
+        if entrada.isalpha():
+            print(f"{cores["vermelho"]}ERRO! {entrada} é inválido.{cores["limpa"]}")
+        else:
+            valido = True
+            return float(entrada)
+    return None
