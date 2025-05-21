ca86ae3ed5e9ce451a772ee48ae8d45b25591dca
Pedro Lazera Jr <pepelazera@gmail.com>
2025-05-21 12:53:18 -0300

    Estudando mais a fundo manipulação de arquivos.

---
+++ app.py
@@ -0,0 +1,26 @@
+# Mode
+# r - Leitura
+# a - Append/Incrementar
+# w - Write 
+# x - Criar Arquivo
+# r+ - Leitura + Escrita
+
+#arquivo = open("AulaArq/teste3.txt", "x") # Caso use o W, ele irá limpar o arquivo anterior e escrever do zero, já o A ele faz uma continuação escrevendo logo abaixo
+
+#print(arquivo.readable()) # Verifica se o arquivo pode ser lido
+#print(arquivo.read()) # Le todas as linhas do arquivo
+#print(arquivo.readline()) # Le a primeira linha do arquivo, e caso coloque outros readline ele lerá as linhas sucetivas
+#print(arquivo.readlines())
+
+#arquivo.write("python\n")
+
+#arquivo.close()
+
+import os
+
+if os.path.exists("AulaArq/teste2.txt"):
+    os.remove("AulaArq/teste2.txt") 
+else:
+    print("Arquivo não existe.")
+
+os.rmdir("AulaArq/nova_pasta")
