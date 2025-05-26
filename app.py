c6cb2c4dcb5fa870e46cf646c42abfca43ce2e57
Pedro Lazera Jr <pepelazera@gmail.com>
2025-05-26 17:40:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,16 @@
 # x - Criar Arquivo
 # r+ - Leitura + Escrita
 
-#arquivo = open("AulaArq/teste3.txt", "x") # Caso use o W, ele irá limpar o arquivo anterior e escrever do zero, já o A ele faz uma continuação escrevendo logo abaixo
+arquivo = open("AulaArq/teste3.txt", "x") # Caso use o W, ele irá limpar o arquivo anterior e escrever do zero, já o A ele faz uma continuação escrevendo logo abaixo
 
-#print(arquivo.readable()) # Verifica se o arquivo pode ser lido
-#print(arquivo.read()) # Le todas as linhas do arquivo
-#print(arquivo.readline()) # Le a primeira linha do arquivo, e caso coloque outros readline ele lerá as linhas sucetivas
-#print(arquivo.readlines())
+print(arquivo.readable()) # Verifica se o arquivo pode ser lido
+print(arquivo.read()) # Le todas as linhas do arquivo
+print(arquivo.readline()) # Le a primeira linha do arquivo, e caso coloque outros readline ele lerá as linhas sucetivas
+print(arquivo.readlines())
 
-#arquivo.write("python\n")
+arquivo.write("python\n")
 
-#arquivo.close()
+arquivo.close()
 
 import os
 
