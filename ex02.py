bb1fddbdaa945a78394b7f7052ae4d6a18538c76
Pedro Lazera Jr <pepelazera@gmail.com>
2025-06-04 14:19:14 -0300

    Exercicio de manipulação de arquivos.

--- ex02.py
+++ ex02.py
@@ -2,10 +2,10 @@ arq = "compras2.txt"
 
 for c in range(1, 3):
     with open(arq, "a") as arquivo:
-        r = str(input(f"Digite o {c}o. alimento e sua quantidade: "))
-        arquivo.write(r + "\n")
-        d = r.replace(",", " ")
-for i in range(1, 3):
-    with open(arq, "r") as arquivo:
-        arquivo.read(d)
-    
+        a = str(input(f"Digite o {c}o. alimento e sua quantidade: "))
+        d = a.replace(",", " ").replace(",  ", " ")
+        arquivo.write(d + "\n")
+
+with open(arq, "r") as arquivo:
+    for linha in arquivo:
+        print("Você precisa comprar", linha.strip())
