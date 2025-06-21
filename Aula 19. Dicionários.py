e2cc68bd3b5a274b76549300b758143ed34997ea
Pedro Lazera Jr <pepelazera@gmail.com>
2025-06-21 17:57:21 -0300

    Update Aula 19. Dicionários.py

--- Aula 19. Dicionários.py
+++ Aula 19. Dicionários.py
@@ -2,3 +2,4 @@ pessoas = {'nome': 'Gustavo', 'sexo': 'Masculino', 'idade': 22} #cria dicionári
 print(pessoas.keys()) #mostra as chaves ex: nome, sexo, idade
 print(pessoas.values()) #mostra os valores ex: Gustavo, Masculino, 22
 print(pessoas.items()) #mostra todos os valores e chaves, ou seja, todos os itens da lista
+
